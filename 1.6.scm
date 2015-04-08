28ee19cf833a89793b51351149da3d441a880118
Jonghwa Woo <wjh0630@gmail.com>
2015-04-08 14:56:47 +0900

    Add 1.4 ~ 1.6

---
+++ 1.6.scm
@@ -0,0 +1,12 @@
+(define (new-if predicate then-clause else-clause)
+  (cond (predicate then-clause)
+        (else else-clause)))
+(new-if (= 2 3) 0 5)
+(new-if (= 1 1) 0 5)
+(define (sqrt-iter guess x)
+  (new-if (good-enough? guess x)
+          guess
+          (sqrt-iter (improve guess x)
+                     x)))
+; (sqrt-iter 1.0 9)
+; This will go into infinite loop. The reason is that all arguments of new-if should be evaluated before new-if is evaluated because it's just a normal procedure.
