d37b4ee891fb7891b67095cf3b66e6d54d06f102
Jonghwa Woo <wjh0630@gmail.com>
2015-04-08 15:10:07 +0900

    Fix bugs

--- 1.5.scm
+++ 1.5.scm
@@ -4,4 +4,6 @@
       0
       y))
 ; (test 0 (p))
-; This will result in an infinite loop because MIT-Scheme uses applicative-order evaluation. If it used normal-order evaluation, the expression would evalute to zero.
+; This will result in an infinite loop because MIT-Scheme uses
+; applicative-order evaluation. If it used normal-order evaluation,
+; the expression would evalute to zero.
--- 1.6.scm
+++ 1.6.scm
@@ -1,12 +1,19 @@
+(load "lib.scm")
 (define (new-if predicate then-clause else-clause)
   (cond (predicate then-clause)
         (else else-clause)))
 (new-if (= 2 3) 0 5)
 (new-if (= 1 1) 0 5)
 (define (sqrt-iter guess x)
+  (define (good-enough? guess x)
+    (< (abs (- (square guess) x)) 0.001))
+  (define (improve guess x)
+    (average guess (/ x guess)))
   (new-if (good-enough? guess x)
           guess
           (sqrt-iter (improve guess x)
                      x)))
 ; (sqrt-iter 1.0 9)
-; This will go into infinite loop. The reason is that all arguments of new-if should be evaluated before new-if is evaluated because it's just a normal procedure.
+; This will go into infinite loop. The reason is that all arguments of new-if
+; should be evaluated before new-if is evaluated because it's just a normal
+; procedure.
