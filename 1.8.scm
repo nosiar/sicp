1bafd4413e213e7a6052a0b1add13ae3c30eba25
Jonghwa Woo <wjh0630@gmail.com>
2015-04-08 16:01:14 +0900

    Add 1.8

---
+++ 1.8.scm
@@ -0,0 +1,12 @@
+(define (cbrt-iter guess x)
+  (define (improve guess x)
+    (/ (+ (* 2 guess) (/ x (square guess))) 3))
+  (define (good-enough? guess x)
+    (= guess (improve guess x)))
+  (if (good-enough? guess x)
+      guess
+      (cbrt-iter (improve guess x)
+                 x)))
+
+(cbrt-iter 1.0 27)
+
