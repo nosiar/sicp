0bb5abeb5165411f502cb4a7c0fb1c613b64a569
Jonghwa Woo <wjh0630@gmail.com>
2015-04-09 09:47:49 +0900

    Add 1.15

---
+++ 1.15.scm
@@ -0,0 +1,11 @@
+(define (cube x) (* x x x))
+(define (p x) (- (* 3 x) (* 4 (cube x))))
+(define (sine angle)
+  (if (not (> (abs angle) 0.1))
+      angle
+      (p (sine (/ angle 3.0)))))
+
+(sine 12.15)
+
+; a. 5 times
+; b. space : O(log(a)), steps : O(log(a)).
