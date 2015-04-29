1cbad8936d70e25e29e377282003382f683a9beb
Jonghwa Woo <wjh0630@gmail.com>
2015-04-29 21:56:22 +0900

    Add 2.6

---
+++ 2.6.scm
@@ -0,0 +1,11 @@
+(define add-1 (lambda (f) (lambda (x) (f ((n f) x)))))
+(define zero (lambda (f) (lambda (x) x)))
+(define one (lambda (f) (lambda (x) (f x))))
+(define two (lambda (f) (lambda (x) (f (f x)))))
+(define (add a b) (lambda (f) (lambda (x) ((a f) ((b f) x)))))
+(define (print-num n) ((n add1) 0))
+
+(print-num zero) ; 0
+(print-num one) ; 1
+(print-num two) ; 2
+(print-num (add (add one two) two)) ; 5
