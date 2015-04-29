ff8d47bce9f1dfa986d968146fe8c9c817b9171e
Jonghwa Woo <wjh0630@gmail.com>
2015-04-29 22:02:22 +0900

    Fix 2.6 bug

--- 2.6.scm
+++ 2.6.scm
@@ -1,4 +1,4 @@
-(define add-1 (lambda (f) (lambda (x) (f ((n f) x)))))
+(define (add-1 n) (lambda (f) (lambda (x) (f ((n f) x)))))
 (define zero (lambda (f) (lambda (x) x)))
 (define one (lambda (f) (lambda (x) (f x))))
 (define two (lambda (f) (lambda (x) (f (f x)))))
