d1fa3e9998264ab0884083ded3dbf7de5dd4a713
Jonghwa Woo <wjh0630@gmail.com>
2015-06-11 10:11:08 +0900

    Add fold-left version

--- 2.39.scm
+++ 2.39.scm
@@ -2,4 +2,16 @@
   (foldr (lambda (x y) (append y (list x))) null sequence))
 
 (define (reverse-y sequence)
-  (foldl (lambda (x y) (cons x y)) null sequence))
+  (foldl cons null sequence))
+
+(define (fold-left op initial sequence)
+  (define (iter result rest)
+    (if (null? rest)
+        result
+        (iter (op result (car rest))
+              (cdr rest))))
+  (iter initial sequence))
+
+(define (reverse-z sequence)
+  (fold-left (lambda (x y) (cons y x)) null sequence))
+
